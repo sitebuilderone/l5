942dc9189828c24bc4671492facdfab68c1862c8
Anthony Lepki <anton@sitebuilderone.com>
2015-05-17 22:37:06 -0700

    Carbon and query scopes

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -17,6 +17,12 @@
 		{!! Form::label('body', 'Body:') !!}
 		{!! Form::textarea('body', null, ['class'=>'form-control']) !!}
 	</div>
+
+	<div class="form-group">
+		{!! Form::label('published_at', 'Published On:') !!}
+		{!! Form::input('date', 'published_at', date('Y-m-d'), ['class'=>'form-control']) !!}
+	</div>
+
 	
 	<div class="form-group">
 		{!! Form::submit('Add Article', null, ['class'=>'btn btn-primary form-control']) !!}
